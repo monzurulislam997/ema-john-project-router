e61dcead6ddd18bc01c94e8dd13f207581a084e1
unknown <monzurulislam997@gmail.com>
2021-10-01 02:29:26 +0600

    code added

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h3>This is an inventory</h3>
+        </div>
+    );
+};
+
+export default Inventory;
